1e93bd546bd3d944c8c3e1ceeb82fe5d7cc4cf91
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-12-20 16:02:01 -0600

    added simple journal

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today");
+            j.AddEntry("I ate a bug");
+            Console.WriteLine(j);
+
+        }
+    }
+}
